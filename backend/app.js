d57e3ea3c576c027976a2d4df9f492fe2f153b2a
NicoJob13 <74857678+NicoJob13@users.noreply.github.com>
2021-08-06 10:50:28 +0200

    Ajout process.env.DIALECT

--- backend/app.js
+++ backend/app.js
@@ -40,7 +40,7 @@ const sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PASS, {
-        dialect: "mysql",
+        dialect: process.env.DIALECT,
         host: process.env.DB_HOST,
         logging: false
     }
--- backend/config/config.js
+++ backend/config/config.js
@@ -6,20 +6,20 @@ module.exports = {
     "password": process.env.DB_PASS,
     "database": process.env.DB_NAME,
     "host": process.env.DB_HOST,
-    "dialect": "mysql"
+    "dialect": process.env.DIALECT
   },
   "test": {
     "username": process.env.DB_USER,
     "password": process.env.DB_PASS,
     "database": process.env.DB_NAME,
     "host": process.env.DB_HOST,
-    "dialect": "mysql"
+    "dialect": process.env.DIALECT
   },
   "production": {
     "username": process.env.DB_USER,
     "password": process.env.DB_PASS,
     "database": process.env.DB_NAME,
     "host": process.env.DB_HOST,
-    "dialect": "mysql"
+    "dialect": process.env.DIALECT
   }
 }
