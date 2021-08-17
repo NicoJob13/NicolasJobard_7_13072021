4f65bb8bc014da538d380a8d6bd4ffdc1215fd1a
NicoJob13 <74857678+NicoJob13@users.noreply.github.com>
2021-08-17 10:58:08 +0200

    Envoi des posts sans Redux

--- frontend/src/actions/postsactions.js
+++ frontend/src/actions/postsactions.js
@@ -2,7 +2,6 @@ import axios from "axios";
 
 // Posts
 export const GET_POSTS = "GET_POSTS";
-export const ADD_POST = "ADD_POST";
 export const UPDATE_POST = "UPDATE_POST";
 export const DELETE_POST = "DELETE_POST";
 
@@ -15,13 +14,6 @@ export const getPosts = (num) => {
                 dispatch({ type: GET_POSTS, payload: array })
             })
             .catch((err) => console.log(err));
-    };
-};
-
-export const addPost = (data) => {
-    return (dispatch) => {
-        return axios
-            .post(`${process.env.REACT_APP_API_URL}/api/posts/`, data);
     };
 };
 
