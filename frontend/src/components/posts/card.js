c65633eb4e26e03f6b507ed19ad50a51a9bf8de4
NicoJob13 <74857678+NicoJob13@users.noreply.github.com>
2021-08-17 21:36:25 +0200

    Edition et suppression des commentaires

--- frontend/src/components/posts/card.js
+++ frontend/src/components/posts/card.js
@@ -11,6 +11,8 @@ const Card = ({ post }) => {
     const [showComments, setShowComments] = useState(false);
     const [comments, setComments] = useState([]);
     const [message, setMessage] = useState('');
+    const [comIsUpdated, setComIsUpdated] = useState(false);
+    const [comUpdate, setComUpdate] = useState(null);
     const usersData = useSelector((state) => state.usersReducer);
     const userData = useSelector((state) => state.userReducer);
     const dispatch = useDispatch();
@@ -36,12 +38,12 @@ const Card = ({ post }) => {
         const pid = post.id;
         console.log(pid);
         axios.get(`${process.env.REACT_APP_API_URL}/api/comments/post/${post.id}`)
-            .then(res => {
-                const newCommentsData = res.data;
-                console.log(newCommentsData);
-                setComments(newCommentsData);
-                return comments;
-            });
+        .then(res => {
+            const newCommentsData = res.data;
+            console.log(newCommentsData);
+            setComments(newCommentsData);
+            return comments;
+        });
     };
 
     const cancelCom = () => {
@@ -70,6 +72,36 @@ const Card = ({ post }) => {
         } else {
             alert("Veuillez entrer un commentaire")
         }
+    };
+
+    const updateCom = (cid) => {
+        if (comUpdate) {
+            axios({
+                method: 'put',
+                url: `${process.env.REACT_APP_API_URL}/api/comments/${cid}`,
+                withCredentials: true,
+                data: {
+                    text : comUpdate,
+                },
+            })
+            .then((res) => {
+                cancelCom();
+                getComments();
+                setComIsUpdated(false);
+            })
+            .catch((err) => console.log(err));
+        }
+    };
+
+    const deleteCom = (cid) => {
+        axios({
+            method: 'delete',
+            url: `${process.env.REACT_APP_API_URL}/api/comments/${cid}`,
+        })
+        .then((res) => {
+            getComments();
+        })
+        .catch((err) => console.log(err));
     };
 
     useEffect(() => {
@@ -129,6 +161,32 @@ const Card = ({ post }) => {
                                     <div className='pt-1'>{dateParser(comment.createdAt)}</div>
                                 </div>
                                 <div>{comment.text}</div>
+                                <div>
+                                    {(userData.id === comment.UserId || userData.role === 'Chargé(e) de communication') && (comIsUpdated === false) && (
+                                    <>
+                                        <div>
+                                            <div onClick={() => setComIsUpdated(true)}><i className="far fa-edit"></i></div>
+                                        </div>
+                                        <div>
+                                            <div onClick={() => {
+                                                if(window.confirm('Vous êtes sur le point de supprimer un commentaire. Confirmez-vous la suppression ?')) {
+                                                    deleteCom(comment.id);
+                                                }
+                                            }}><i className="far fa-trash-alt"></i>
+                                            </div>
+                                        </div>
+                                    </>
+                                    )}
+                                    {(userData.id === comment.UserId || userData.role === 'Chargé(e) de communication') && comIsUpdated && (
+                                        <div>
+                                            <input type='text' className='form-control' defaultValue={comment.text} onChange={(e) => setComUpdate(e.target.value)} />
+                                            <div>
+                                                <button onClick={() => updateCom(comment.id)}>Valider</button>
+                                                <button onClick={() => setComIsUpdated(false)}>Annuler</button>
+                                            </div>
+                                        </div>
+                                    )}
+                                </div>
                             </div>
                         ))}
                     </div>
